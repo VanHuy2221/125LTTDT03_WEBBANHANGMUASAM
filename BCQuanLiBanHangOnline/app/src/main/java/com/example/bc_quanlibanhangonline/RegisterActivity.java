3d7ae80251c1fcf3ec44a1fdb75bf1375c4bbbec
minhchinh595 <minhchinh595@gmail.com>
2025-12-23 23:41:34 +0700

    Chỉnh sửa giao diện đăng nhập, đăng ký

---
+++ BCQuanLiBanHangOnline/app/src/main/java/com/example/bc_quanlibanhangonline/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.bc_quanlibanhangonline;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Chỉ hiển thị layout, không xử lý sự kiện gì cả
+        // Các sự kiện sẽ được thêm sau
+    }
+}
