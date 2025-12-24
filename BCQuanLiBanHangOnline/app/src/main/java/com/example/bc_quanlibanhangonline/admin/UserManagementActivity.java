756bcbf61bb71dcb4ba98736eea2fa358e8a327a
VanHuy2221 <23115053122221@sv.ute.udn.vn>
2025-12-25 01:59:55 +0700

    Thêm giao diện

--- BCQuanLiBanHangOnline/app/src/main/java/com/example/bc_quanlibanhangonline/admin/UserManagementActivity.java
+++ BCQuanLiBanHangOnline/app/src/main/java/com/example/bc_quanlibanhangonline/admin/UserManagementActivity.java
@@ -1,7 +1,9 @@
-package com.example.bc_quanlibanhangonline;
+package com.example.bc_quanlibanhangonline.admin;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bc_quanlibanhangonline.R;
 
 public class UserManagementActivity extends AppCompatActivity {
 
